58a9034430105644972bc2bdab14f437660cbea0
Carlos <carlos96831@gmail.com>
2021-09-14 21:59:27 -0700

    added scripting

---
+++ assets/script.js
@@ -0,0 +1,21 @@
+var button = document.querySelector('.button');
+var inputValue = document.querySelector('.inputValue');
+var name = document.querySelector('.name');
+var desc = document.querySelector('.desc');
+var temp = document.querySelector('.temp');
+
+button.addEventListener('click',function(){
+    fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid=78b4d98931a5629b9470317fc4512592')
+    .then(response => Response.json())
+    .then(data => {
+        var nameValue = data['name'];
+        var tempValue = data['main']['temp'];
+        var descValue = data['weather'][0]['description'];
+
+        name.innerHTML =nameValue;
+        temp.innerHTML =tempValue;
+        desc.innerHTML =descValue;
+    })
+
+    .catch(err => alert("Wrong city name!"))
+})
