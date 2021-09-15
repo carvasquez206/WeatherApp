4a913a6771e0be417e1f4db135b7802a455ab1e6
Carlos <carlos96831@gmail.com>
2021-09-14 22:34:18 -0700

    edited the files

--- assets/script.js
+++ assets/script.js
@@ -8,13 +8,15 @@ button.addEventListener('click',function(){
     fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid=78b4d98931a5629b9470317fc4512592')
     .then(response => Response.json())
     .then(data => {
+
         var nameValue = data['name'];
         var tempValue = data['main']['temp'];
         var descValue = data['weather'][0]['description'];
 
-        name.innerHTML =nameValue;
-        temp.innerHTML =tempValue;
-        desc.innerHTML =descValue;
+        name.innerHTML =nonmreValue
+        temp.innerHTML =tempValue
+        desc.innerHTML =descValue
+
     })
 
     .catch(err => alert("Wrong city name!"))
