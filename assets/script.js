a89a6d7e3b66394a2a93d8f14c4cbdcdb54f40bc
Carlos <carlos96831@gmail.com>
2021-09-15 21:43:19 -0700

    changed script

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,59 @@
-var button = document.querySelector('.button');
-var inputValue = document.querySelector('.inputValue');
-var name = document.querySelector('.name');
-var desc = document.querySelector('.desc');
-var temp = document.querySelector('.temp');
+function GetInfo() {
 
-button.addEventListener('click',function(){
-    fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid=78b4d98931a5629b9470317fc4512592')
-    .then(response => Response.json())
-    .then(data => {
+    var newName = document.getElementById("cityInput");
+    var cityName = document.getElementById("cityName");
+    cityName.innerHTML = "--"+newName.value+"--";
 
-        var nameValue = data['name'];
-        var tempValue = data['main']['temp'];
-        var descValue = data['weather'][0]['description'];
+fetch('https://api.openweathermap.org/data/2.5/forecast?q='+newName.value+'&appid=32ba0bfed592484379e51106cef3f204')
+.then(response => response.json())
+.then(data => {
 
-        name.innerHTML =nameValue
-        temp.innerHTML =tempValue
-        desc.innerHTML =descValue
+    //Getting the min and max values for each day
+    for(i = 0; i<5; i++){
+        document.getElementById("day" + (i+1) + "Min").innerHTML = "Min: " + Number(data.list[i].main.temp_min - 273.15).toFixed(1)+ "°";
+        //Number(1.3450001).toFixed(2); // 1.35
+    }
 
-    })
+    for(i = 0; i<5; i++){
+        document.getElementById("day" + (i+1) + "Max").innerHTML = "Max: " + Number(data.list[i].main.temp_max - 273.15).toFixed(2) + "°";
+    }
+    //------------------------------------------------------------
 
-    .catch(err => alert("Wrong city name!"))
+    //Getting Weather Icons
+     for(i = 0; i<5; i++){
+        document.getElementById("img" + (i+1)).src = "http://openweathermap.org/img/wn/"+
+        data.list[i].weather[0].icon
+        +".png";
+    }
+    //------------------------------------------------------------
+    console.log(data)
+
+
 })
+
+.catch(err => alert("Something Went Wrong: Try Checking Your Internet Coneciton"))
+}
+
+function DefaultScreen(){
+    document.getElementById("cityInput").defaultValue = "Seattle";
+    GetInfo();
+}
+
+
+//Getting and displaying the text for the upcoming five days of the week
+var d = new Date();
+var weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday",];
+
+//Function to get the correct integer for the index of the days array
+function CheckDay(day){
+    if(day + d.getDay() > 6){
+        return day + d.getDay() - 7;
+    }
+    else{
+        return day + d.getDay();
+    }
+}
+
+    for(i = 0; i<5; i++){
+        document.getElementById("day" + (i+1)).innerHTML = weekday[CheckDay(i)];
+    }
